4ed1181b3030b14173662588f88121d13bfee203
Akira Kosuda <sayuen0@gmail.com>
2019-02-01 14:06:23 +0900

    fixed conflict 1

--- app/controllers/greets_controller.rb
+++ app/controllers/greets_controller.rb
@@ -2,12 +2,8 @@ class GreetsController < ApplicationController
   PER=8
 
   def hello
-<<<<<<< HEAD
     @members = Member.page(params[:page]).per(PER)
-=======
-    @members = Member.order(:id).limit(3)
->>>>>>> origin/master
-  end
+t   end
 
 
   def members
